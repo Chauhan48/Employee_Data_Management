622342904990e603885d00263aa5f46414d7fb3a
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-29 15:50:03 +0530

    feat: add edit and delete button to card

--- client/src/components/EmployeeCard.jsx
+++ client/src/components/EmployeeCard.jsx
@@ -1,6 +1,17 @@
 import { Card, CardActionArea, Typography, Button } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 export default function EmployeeCard({ employeeData }) {
+
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = () => {
+        
+    }
+
     return (
         <>
             <Card sx={{ maxWidth: 345, margin: 'auto', m: 2, boxShadow: 4 }}>
@@ -10,8 +21,17 @@ export default function EmployeeCard({ employeeData }) {
                     <Typography variant="body1" color="text.secondary">Email: {employeeData.email}</Typography>
                     <Typography variant="body1" color="text.secondary">Position: {employeeData.position}</Typography>
                 </CardActionArea>
-                <Button>Edit</Button>
-                <Button>Delete</Button>
+                <Button
+                    variant='outlined'
+                    startIcon={<EditIcon />}
+                    fullWidth sx={{ mb: 1, borderRadius: 1, textTransform: 'none', fontWeight: '500' }}
+                    onClick={handleEdit}>Edit</Button>
+                <Button
+                    variant='outlined'
+                    color="error"
+                    startIcon={<DeleteIcon />}
+                    fullWidth sx={{ borderRadius: 1, textTransform: 'none', fontWeight: '500' }}
+                    onClick={handleDelete}>Delete</Button>
             </Card>
         </>
     )
