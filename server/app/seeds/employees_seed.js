c350d82c1fd965f90ded4a769c8f0ab9df525aeb
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-30 23:17:59 +0530

    add values to seed data

--- server/app/seeds/employees_seed.js
+++ server/app/seeds/employees_seed.js
@@ -9,6 +9,10 @@ exports.seed = async function (knex) {
     { name: "Rohit Sharma", email: "rohit@gmail.com", position: "Manager" },
     { name: "Priya Singh", email: "priya@gmail.com", position: "Designer" },
     { name: "Amit Verma", email: "amit@gmail.com", position: "HR" },
-    { name: "Sneha Gupta", email: "sneha@gmail.com", position: "SD2" }
+    { name: "Sneha Gupta", email: "sneha@gmail.com", position: "SD2" },
+    { name: "Ayushi Singh", email: "ayushi@gmail.com", position: "Intern" },
+    { name: "Arun Thakur", email: "arun@gmail.com", position: "Designer" },
+    { name: "Ronnin Verma", email: "ronnin@gmail.com", position: "HR" },
+    { name: "Dev Raj", email: "dev@gmail.com", position: "SD2" }
   ]);
 };
